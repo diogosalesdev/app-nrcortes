9b6e127b987f18fecfbeddd5bfa71a9163f7bfa9
Diogo Sales <diogosalesdev@gmail.com>
2020-11-09 11:45:51 -0300

    App authenticate created

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,20 +1,21 @@
 import 'react-native-gesture-handler';
 
 import React from 'react';
-import { View, StatusBar} from 'react-native';
+import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import Routes from './routes';
+import AppProvider from './hooks';
 
-const App: React.FC = () => {
-  return (
-    <NavigationContainer>
-      <StatusBar barStyle="dark-content" backgroundColor="#f2f2f2" />
-      <View  style={{ flex: 1, backgroundColor: '#f2f2f2' }}>
+const App: React.FC = () => (
+  <NavigationContainer>
+    <StatusBar barStyle="dark-content" backgroundColor="#f2f2f2" />
+    <AppProvider>
+      <View style={{ flex: 1, backgroundColor: '#f2f2f2' }}>
         <Routes />
       </View>
-    </NavigationContainer>
-  );
-};
+    </AppProvider>
+  </NavigationContainer>
+);
 
 export default App;
